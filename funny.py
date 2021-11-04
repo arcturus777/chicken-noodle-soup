a2d522c7bc8f2ed5a72f370a743660e2a4061361
arcturus777 <82037480+arcturus777@users.noreply.github.com>
2021-11-04 11:51:22 -0400

    Add files via upload

---
+++ funny.py
@@ -0,0 +1,20 @@
+print "Hello, I live in Australia. I like tea, but not peppermint. My monkey will eat it. I have to go feed my monkey. I'm sorry. Adios."
+name = raw_input("What is your name?")
+if name == 'Sophia':
+	print "How are you darling?"
+elif name == 'Nolan':
+	print "You are also dumb."
+else:
+	print "Afrocircus, afrocircus, polka dot, polka dot, afrocircus."
+
+answer = raw_input("Do you have a question for me?")
+
+if answer == 'yes':
+	ques = raw_input("What is your question?")
+else:
+	print "That's too bad"
+
+if ques == "do you have money?":
+	print "I have 0 thousand dollars."
+else:
+	print "That is a dumb question."
